187ba2380a490a416e414d74bd54d3a8efc8c0f3
Abhishek Sarkar <sarkar.abhishek06@gmail.com>
2022-03-16 00:46:15 +0530

    Adding  the demo project to git!

---
+++ DemoProjectbyRahulShetty/src/Basics.java
@@ -0,0 +1,82 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
+import files.ReUsableMethods;
+import files.payload;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// validate if Add Place API is workimg as expected 
+		//Add place-> Update Place with New Address -> Get Place to validate if New address is present in response
+		
+		//given - all input details 
+		//when - Submit the API -resource,http method
+		//Then - validate the response
+		RestAssured.baseURI= "https://rahulshettyacademy.com";
+		
+		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
+		.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+		
+		System.out.println(response);
+		
+		JsonPath js=new JsonPath(response); //for parsing Json
+		String placeId=js.getString("place_id");
+		
+		System.out.println(placeId);
+		
+		//Update Place
+		String newAddress = "Summer Walk, Africa";
+		
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n" + 
+				"\"place_id\":\""+placeId+"\",\r\n" + 
+				"\"address\":\""+newAddress+"\",\r\n" + 
+				"\"key\":\"qaclick123\"\r\n" + 
+				"}").
+		when().put("maps/api/place/update/json")
+		.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		
+		//Get Place
+		
+	String getPlaceResponse=	given().log().all().queryParam("key", "qaclick123")
+		.queryParam("place_id",placeId)
+		.when().get("maps/api/place/get/json")
+		.then().assertThat().log().all().statusCode(200).extract().response().asString();
+	JsonPath js1=ReUsableMethods.rawToJson(getPlaceResponse);
+	String actualAddress =js1.getString("address");
+	System.out.println(actualAddress);
+	Assert.assertEquals(actualAddress, "Pacific ocean");
+	//Cucumber Junit, Testng
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ DemoProjectbyRahulShetty/src/files/payload.java
@@ -0,0 +1,33 @@
+package files;
+
+public class payload {
+
+	
+	public static String AddPlace()
+	
+	{
+		return "{\r\n" + 
+				"  \"location\": {\r\n" + 
+				"    \"lat\": -38.383494,\r\n" + 
+				"    \"lng\": 33.427362\r\n" + 
+				"  },\r\n" + 
+				"  \"accuracy\": 50,\r\n" + 
+				"  \"name\": \"Rahul Shetty Academy\",\r\n" + 
+				"  \"phone_number\": \"(+91) 983 893 3937\",\r\n" + 
+				"  \"address\": \"29, side layout, cohen 09\",\r\n" + 
+				"  \"types\": [\r\n" + 
+				"    \"shoe park\",\r\n" + 
+				"    \"shop\"\r\n" + 
+				"  ],\r\n" + 
+				"  \"website\": \"http://rahulshettyacademy.com\",\r\n" + 
+				"  \"language\": \"French-IN\"\r\n" + 
+				"}\r\n" + 
+				"";
+		
+		
+	}
+	
+
+	
+	
+}
